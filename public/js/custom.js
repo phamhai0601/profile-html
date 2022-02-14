37e12324956c406c50b3531bf09a0d274b92a032
mitto <mitto.hai.7356@gmail.com>
2022-02-14 08:47:38 +0700

    first commit

---
+++ public/js/custom.js
@@ -0,0 +1,11 @@
+$(document).on('click', 'button[class="navbar-toggler d-xs-block d-sm-none"]', function() {
+	var div = $('div[class="col-xs-8 col-sm-4 d-flex flex-column px-5 d-none d-xs-none d-sm-block"]');
+	if(div.css('display') == 'block') {
+		div.attr("style", "display:none!important");
+		$(this).html('<i class="fas fa-bars"></i>')
+	} else {
+		div.attr("style", "display:block!important");
+		$(this).html('<i class="fas fa-times"></i>')
+	}
+})
+
